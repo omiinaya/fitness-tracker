6c12ee9523152f49594f5192d3209cf204d41bd7
Omar Minaya <omiinaya@gmail.com>
2020-05-14 04:48:18 -0400

    added /workouts/range route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,6 +11,16 @@ module.exports = function(app){
         })
     });
 
+    app.get("/api/workouts/range",function(req,res){  
+        Workout.find()
+        .then(data =>{  
+            res.json(data)
+        })
+        .catch(err => { 
+            res.json(err)
+        })
+    });
+
     app.post("/api/workouts",function (req,res){    
         Workout.create({})
         .then(data => res.json(data))
