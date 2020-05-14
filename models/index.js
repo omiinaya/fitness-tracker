91920898686558746959a826462aba7314465f16
Omar Minaya <omiinaya@gmail.com>
2020-05-14 04:29:00 -0400

    creating model for db setup.

---
+++ models/index.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workout_db = new Schema(
+    {
+      day: {
+        type: Date,
+        default: () => new Date()
+      },
+      exercises: [
+        {
+          type: {
+            type: String,
+            trim: true,
+            required: "Enter an exercise type"
+          },
+          name: {
+            type: String,
+            trim: true,
+            required: "Enter an exercise name"
+          },
+          duration: {
+            type: Number,
+            required: "Enter an exercise duration in minutes"
+          },
+          weight: {
+            type: Number
+          },
+          reps: {
+            type: Number
+          },
+          sets: {
+            type: Number
+          },
+          distance: {
+            type: Number
+          }
+        }
+      ]
+    },
+    {
+      toJSON: {
+        virtuals: true
+      }
+    }
+  );
+
+  workout_db.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+      return total + exercise.duration;
+    }, 0);
+  });
+  
+  const Workout = mongoose.model("Workout", workout_db);
+  
+  module.exports = Workout;
