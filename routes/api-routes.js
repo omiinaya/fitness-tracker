ee7afa08055c83c5d6be2bf1c72144f2fdbbb02f
Omar Minaya <omiinaya@gmail.com>
2020-05-14 04:34:08 -0400

    added route for finding workouts in db.

---
+++ routes/api-routes.js
@@ -0,0 +1,13 @@
+const Workout = require("../models /index")
+
+module.exports = function(app){ 
+    app.get("/api/workouts",function(req,res){  
+        Workout.find()
+        .then(data =>{  
+            res.json(data)
+        })
+        .catch(err => { 
+            res.json(err)
+        })
+    });
+}
