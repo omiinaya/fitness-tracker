d8794c10647d0b05a1983f16de7921dca1032175
Omar Minaya <omiinaya@gmail.com>
2020-05-14 04:50:25 -0400

    added route for updating by id.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 const Workout = require("../models /index")
 
 module.exports = function(app){ 
+    //find
     app.get("/api/workouts",function(req,res){  
         Workout.find()
         .then(data =>{  
@@ -10,7 +11,6 @@ module.exports = function(app){
             res.json(err)
         })
     });
-
     app.get("/api/workouts/range",function(req,res){  
         Workout.find()
         .then(data =>{  
@@ -20,9 +20,28 @@ module.exports = function(app){
             res.json(err)
         })
     });
-
+    //create
     app.post("/api/workouts",function (req,res){    
         Workout.create({})
+        .then(data => res.json(data))
+        .catch(err => { 
+            res.json(err)
+        })
+    });
+    app.post("/api/workouts/range",function (req,res){    
+        Workout.create({})
+        .then(data => res.json(data))
+        .catch(err => { 
+            res.json(err)
+        })
+    });
+    //update
+    app.put("/api/workouts/:id",({body,params},res)=>{   
+        Workout.findByIdAndUpdate(  
+         params.id,
+         {$push:{exercises:body} },
+         {new: true,runValidators:true }
+        )
         .then(data => res.json(data))
         .catch(err => { 
             res.json(err)
