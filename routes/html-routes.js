72c7d6c9dc0d5149d1685b8fba3749302912a63e
Omar Minaya <omiinaya@gmail.com>
2020-05-14 04:52:52 -0400

    added excercise route.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,11 @@
 var path = require("path");
 
-app.get("/",function(req,res){    
-    res.sendFile(path.join(__dirname,"../public/index.html"));
-});
+module.exports = function(app){ 
+    app.get("/",function(req,res){    
+        res.sendFile(path.join(__dirname,"../public/index.html"));
+    });
+
+    app.get("/exercise",function (req,res){   
+        res.sendFile(path.join(__dirname,"../public/exercise.html"));
+    });
+}
